cf8bd9f3fb2e644549751dac4c550d8dc996545d
Rufat Safiullin <stud0000229588@study.utmn.ru>
2022-10-02 12:37:33 +0500

    refactoring

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -1,37 +1,32 @@
+import updateModel from "./../utils/updateModel.js";
+
 function init(getData) {
-    
-    const radioBtns = document.querySelectorAll('input[name="program"]')
-    const { base, it, gov, zero } = getData().programs
-    
-    document.querySelector('#base-value').value = base
-    document.querySelector('#it-value').value = it
-    document.querySelector('#gov-value').value = gov
-    document.querySelector('#zero-value').value = zero
+  const radioBtns = document.querySelectorAll('input[name="program"]');
+  const { base, it, gov, zero } = getData().programs;
 
-    document.querySelector('#base-text').textContent = `${base * 100}%`
-    document.querySelector('#it-text').textContent = `${it * 100}%`
-    document.querySelector('#gov-text').textContent = `${gov * 100}%`
-    document.querySelector('#zero-text').textContent = `${zero * 100}%`
+  document.querySelector("#base-value").value = base;
+  document.querySelector("#it-value").value = it;
+  document.querySelector("#gov-value").value = gov;
+  document.querySelector("#zero-value").value = zero;
 
+  document.querySelector("#base-text").textContent = `${base * 100}%`;
+  document.querySelector("#it-text").textContent = `${it * 100}%`;
+  document.querySelector("#gov-text").textContent = `${gov * 100}%`;
+  document.querySelector("#zero-text").textContent = `${zero * 100}%`;
 
-    radioBtns.forEach(radioBtn => {
-        radioBtn.addEventListener('change', (event) => {
-            console.log(event.target);
-            console.log(parseFloat(event.target.value))
-            console.log(event.target.id)
+  radioBtns.forEach((radioBtn) => {
+    radioBtn.addEventListener("change", (event) => {
+      console.log(event.target);
+      console.log(parseFloat(event.target.value));
+      console.log(event.target.id);
 
-            radioBtn.dispatchEvent(new CustomEvent('updateForm', {
-                bubbles: true,
-                detail: {
-                    selectedProgram: parseFloat(event.target.value),
-                    onUpdate: 'radioProgram',
-                    id: event.target.id
-                }
-            }))
-
-
-        })
+      updateModel(event.target, {
+		onUpdate: "radioProgram",
+        selectedProgram: parseFloat(event.target.value),
+        id: event.target.id,
+      });
     });
+  });
 }
 
-export default init
+export default init;
