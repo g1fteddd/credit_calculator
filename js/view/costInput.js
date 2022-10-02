5ca1f30369dd1112a2ef66e642cb0ef7b3b1a24d
Rufat Safiullin <stud0000229588@study.utmn.ru>
2022-10-02 14:45:17 +0500

    add validation costInput

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,13 +1,40 @@
 function init(getData) {
+    const data = getData()
   const input = document.querySelector("#input-cost");
 
-  var cleave = new Cleave(input, {
+  const cleaveInput = new Cleave(input, {
         numeral: true,
         numeralThousandsGroupStyle: "thousand",
         delimiter: " "
   });
+  cleaveInput.setRawValue(data.cost)
 
-  console.log(cleave)
+
+  input.addEventListener('input', () => {
+    const value = +cleaveInput.getRawValue()
+    console.log(value);
+    
+    if (value < data.minPrice || value > data.maxPrice) {
+        input.closest('.param__details').classList.add('param__details--error')
+    } else {
+        input.closest('.param__details').classList.remove('param__details--error')
+    }
+  })
+
+  input.addEventListener('change', () => {
+    const value = +cleaveInput.getRawValue()
+
+    if (value < data.minPrice) {
+        input.closest('.param__details').classList.remove('param__details--error')
+        cleaveInput.setRawValue(data.minPrice)
+    }
+
+    if (value > data.maxPrice) {
+        input.closest('.param__details').classList.remove('param__details--error')
+        cleaveInput.setRawValue(data.maxPrice)
+    }
+  })
+  
 }
 
 export default init;
